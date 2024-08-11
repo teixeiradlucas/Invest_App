dda3839a31bb6c58b5fcb4542f7031d42071e230
Lucas Teixeira <lucas.francisco.teixeira7@gmail.com>
2024-08-11 19:32:15 -0300

    Adicionando componentes a home

--- lib/app/presentation/pages/home/home_page.dart
+++ lib/app/presentation/pages/home/home_page.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:invest_app/app/presentation/pages/home/widgets/app_bar_widget.dart';
+import 'package:invest_app/app/presentation/pages/home/widgets/stock_item.dart';
+import 'package:invest_app/app/utils/dimension_custom.dart';
+import 'package:invest_app/app/utils/themes/app_colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      drawer: Drawer(),
+    return Scaffold(
+      drawer: const Drawer(),
       body: Column(
         children: [
-          AppBarWidget(),
+          const AppBarWidget(),
+          Container(
+            color: AppColors.whiteColor,
+            width: 500,
+            height: 200,
+          ),
+          gap,
+          const StockItem(),
         ],
       ),
     );
