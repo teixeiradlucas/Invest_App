e66ae7688d657b39c94453e3a17ab3dfec5a3a4b
Lucas Teixeira <lucas.francisco.teixeira7@gmail.com>
2024-08-11 19:03:26 -0300

    Adicionando fontes

---
+++ lib/app/utils/themes/app_fonts.dart
@@ -0,0 +1,6 @@
+class AppFonts {
+  static const String poppins = 'Poppins';
+  static const String sallim = 'Sallim';
+  static const String inter = 'Inter';
+  static const String interBold = 'InterBold';
+}
