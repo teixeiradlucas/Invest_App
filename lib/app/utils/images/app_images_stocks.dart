306a5140ed8b628adb222928c175ad79e0314b56
Lucas Teixeira <lucas.francisco.teixeira7@gmail.com>
2024-08-17 17:29:26 -0300

    adicionando map para imagens das ações

---
+++ lib/app/utils/images/app_images_stocks.dart
@@ -0,0 +1,17 @@
+final Map<String, String> ticketImages = {
+  'BBAS3': AppImagesStocks.bbas3,
+  'ABEV3': AppImagesStocks.abev3,
+  'AURE33': AppImagesStocks.aure3,
+  'B3SA3': AppImagesStocks.b3sa3,
+  'BBDC3': AppImagesStocks.bbdc3,
+  'BBDC4': AppImagesStocks.bbdc3,
+};
+
+class AppImagesStocks {
+  static const defaultImage = 'assets/images/stocks/b3.png';
+  static const bbas3 = 'assets/images/stocks/bbas3.jpg';
+  static const abev3 = 'assets/images/stocks/abev3.jpg';
+  static const aure3 = 'assets/images/stocks/aure3.jpg';
+  static const b3sa3 = 'assets/images/stocks/b3sa3.jpeg';
+  static const bbdc3 = 'assets/images/stocks/bbdc3.jpg';
+}
