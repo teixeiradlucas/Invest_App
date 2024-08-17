397f8dc64f474a9e09727071658a49d3fd6dfeac
Lucas Teixeira <lucas.francisco.teixeira7@gmail.com>
2024-08-17 14:04:17 -0300

    Implementando api

--- lib/app/data/api_url.dart
+++ lib/app/data/api_url.dart
@@ -1,3 +1,3 @@
-const apiUrl = 'http://mfinance.com.br/api/v1/stocks';
+const apiUrlStocks = 'http://mfinance.com.br/api/v1/stocks';
 const apiUrlHistoricals = 'https://mfinance.com.br/api/v1/stocks/historicals/';
 const apiUrlDividends = 'https://mfinance.com.br/api/v1/stocks/dividends/';
