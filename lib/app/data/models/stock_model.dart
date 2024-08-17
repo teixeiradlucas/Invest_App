2ee8d4664813138477c078072f23caa33289af3c
Lucas Teixeira <lucas.francisco.teixeira7@gmail.com>
2024-08-17 17:28:51 -0300

    adicionando componentes no model

--- lib/app/data/models/stock_model.dart
+++ lib/app/data/models/stock_model.dart
@@ -1,9 +1,11 @@
 class StockModel {
   StockModel({
+    required this.priceOpen,
     required this.ticket,
     required this.name,
     required this.sector,
-    required this.value,
+    required this.price,
+    required this.volume,
     required this.dividendYield,
     required this.lastYearHigh,
     required this.lastYearLow,
@@ -14,7 +16,9 @@ class StockModel {
       ticket: json['symbol'] as String,
       name: json['name'] as String,
       sector: json['sector'] as String,
-      value: (json['lastPrice'] as num).toDouble(),
+      price: (json['lastPrice'] as num).toDouble(),
+      priceOpen: (json['priceOpen'] as num).toDouble(),
+      volume: json['volume'] as int,
       dividendYield: (json['dividendYield'] as num).toDouble(),
       lastYearHigh: (json['lastYearHigh'] as num).toDouble(),
       lastYearLow: (json['lastYearLow'] as num).toDouble(),
@@ -24,8 +28,10 @@ class StockModel {
   final String ticket;
   final String name;
   final String sector;
-  final double value;
+  final double price;
+  final int volume;
   final double dividendYield;
   final double lastYearHigh;
   final double lastYearLow;
+  final double priceOpen;
 }
