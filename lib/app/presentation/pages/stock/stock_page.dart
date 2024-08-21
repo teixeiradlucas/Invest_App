8c8f3bb2dbad27d403ff17d227654bb9183ebeab
Lucas Teixeira <lucas.francisco.teixeira7@gmail.com>
2024-08-21 14:59:34 -0300

    Melhorias na Stock Pages

--- lib/app/presentation/pages/stock/stock_page.dart
+++ lib/app/presentation/pages/stock/stock_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:invest_app/app/data/models/stock_model.dart';
 import 'package:invest_app/app/presentation/pages/stock/widgets/app_bar_stock_widget.dart';
 import 'package:invest_app/app/presentation/pages/stock/widgets/historical_widget.dart';
+import 'package:invest_app/app/presentation/pages/stock/widgets/rectangle_widget.dart';
+import 'package:invest_app/app/utils/dimension_custom.dart';
+import 'package:invest_app/app/utils/formatted_value.dart';
 
 class StockPage extends StatelessWidget {
   const StockPage({
@@ -17,6 +20,35 @@ class StockPage extends StatelessWidget {
       body: Column(
         children: [
           AppBarStockWidget(stock: stock),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RectangleWidget(
+                type: 'VALOR',
+                value: formattedPrice(stock.price),
+              ),
+              gap,
+              RectangleWidget(
+                type: 'DY',
+                value: formattedPercentage(stock.dividendYield),
+              ),
+            ],
+          ),
+          gap,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RectangleWidget(
+                type: 'MAXIMA(12M)',
+                value: formattedPrice(stock.lastYearHigh),
+              ),
+              gap,
+              RectangleWidget(
+                type: 'MINIMA(12M)',
+                value: formattedPrice(stock.lastYearLow),
+              ),
+            ],
+          ),
           HistoricalWidget(stock: stock),
         ],
       ),
